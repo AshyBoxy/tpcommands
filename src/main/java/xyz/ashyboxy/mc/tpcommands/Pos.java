73e1c025416485d6747755a2c07307da88f5aff7
AshyBoxy <ashyboxy@ashyboxy.xyz>
2025-02-02 17:38:01 +0000

    1.21.4

--- src/main/java/xyz/ashyboxy/mc/tpcommands/Pos.java
+++ src/main/java/xyz/ashyboxy/mc/tpcommands/Pos.java
@@ -4,13 +4,15 @@ import java.util.Collections;
 
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Relative;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.properties.DoorHingeSide;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
 
 public record Pos(Vec3 pos, Vec2 rot, ResourceKey<Level> dimension) {
     public boolean teleport(ServerPlayer player) {
         return player.teleportTo(player.getServer().getLevel(this.dimension()), this.pos().x, this.pos().y,
-                this.pos().z, Collections.emptySet(), this.rot().y, this.rot().x);
+                this.pos().z, Collections.emptySet(), this.rot().y, this.rot().x, true);
     }
 }
